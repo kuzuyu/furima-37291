1b82eb609187407fa27c4d3edbf565ac0531acad
kuzuyu <95894334+kuzuyu@users.noreply.github.com>
2022-01-24 11:31:45 +0900

    カラムの追加・編集

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,11 @@ ActiveRecord::Schema.define(version: 2022_01_21_045451) do
     t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "family_name", default: "", null: false
+    t.string "first_name_kana", default: "", null: false
+    t.string "family_name_kana", default: "", null: false
+    t.date "birth_day"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
