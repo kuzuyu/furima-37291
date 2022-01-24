124baa2411223f477a400436486e638021cb300c
kuzuyu <95894334+kuzuyu@users.noreply.github.com>
2022-01-24 18:49:15 +0900

    Update user.rb

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     name {Faker::Name.last_name}
     email {Faker::Internet.free_email}
     password = '1a' +Faker::Internet.password(min_length: 6)
+    password {password}
     password_confirmation {password}
     family_name {"田中"}
     first_name {"太郎"}
