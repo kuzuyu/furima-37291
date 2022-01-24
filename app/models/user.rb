602d85de69ebb14f59dee52bf0ab0dff04867fdc
kuzuyu <95894334+kuzuyu@users.noreply.github.com>
2022-01-24 11:33:00 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
+  validates :family_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_day, presence: true
 end
