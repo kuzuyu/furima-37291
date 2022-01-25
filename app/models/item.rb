aa4dc45f3f41cf5210d6a385b0221bbcd4a6cb1d
kuzuyu <95894334+kuzuyu@users.noreply.github.com>
2022-01-26 05:19:05 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,14 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :item_condition
+  belongs_to :postage_payer
+  belongs_to :area
+  belongs_to :preparation_day
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :postage_payer_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :area_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :preparation_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
