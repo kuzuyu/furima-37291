06fd67afbeb8b9a6c56c413b90073cab50af0c01
kuzuyu <95894334+kuzuyu@users.noreply.github.com>
2022-01-25 19:10:57 +0900

    カテゴリーの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,26 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end    
   end
 
+  private
+
+  def item_params
+    params.require(:item).permit(:category_id)
+  end
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,5 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
 
-  validates :item_name, presence: true
-
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
