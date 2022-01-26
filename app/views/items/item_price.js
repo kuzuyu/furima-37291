0855641aa356a953b7de6e1a7c9f97b010e6780f
kuzuyu <95894334+kuzuyu@users.noreply.github.com>
2022-01-26 19:52:52 +0900

    販売価格の計算の実装

---
+++ app/views/items/item_price.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+
+  const priceInput = document.getElementById("price");
+
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
+    const profitNumber = document.getElementById("profit")
+    const value_result = inputValue * 0.1
+    profitNumber.innerHTML = (Math.floor(inputValue - value_result));
+  })
+});
