9919c7bca0a591208e3976981f4ea5a4bbacbbc8
kuzuyu <95894334+kuzuyu@users.noreply.github.com>
2022-01-28 16:40:16 +0900

    Items_controllerの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,7 +21,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:category_id)
+    params.require(:item).permit(:item_name, :item_information, :price, :category_id, :item_condition_id, :postage_payer_id, :area_id, :preparation_day_id, :image ).merge(user_id: current_user.id)
   end
   
 end
