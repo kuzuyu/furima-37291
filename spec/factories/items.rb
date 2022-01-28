afc48436378da358171673c2298e459141c81c4d
kuzuyu <95894334+kuzuyu@users.noreply.github.com>
2022-01-28 16:41:37 +0900

    テストコードの設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name           {"名前"}
+    item_information    {"説明"}
+    price               {10000}
+    item_condition_id       {1}
+    postage_payer_id        {1}
+    preparation_day_id      {1}
+    area_id                 {1}
+    category_id             {1}
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  describe '商品情報入力' do
+    context '商品情報の入力がうまくいかない時' do
+      it 'imageが空の場合' do
+        @item.image = 
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'item_nameが空の場合' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+      it 'item_informationが空の場合' do
+        @item.item_information = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item information can't be blank")
+      end
+      it 'priceが空の場合' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'categoryが空の場合' do
+        @item.category_id = 
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'item_conditionが空の場合' do
+        @item.item_condition_id = 
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item condition can't be blank")
+      end
+      it 'postage_payerが空の場合' do
+        @item.postage_payer_id = 
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Postage payer can't be blank")
+      end
+      it 'prefectureが空の場合' do
+        @item.preparation_day_id = 
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Preparation day can't be blank")
+      end
+      it 'areaが空の場合' do
+        @item.area_id = 
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area can't be blank")
+      end
+      it 'priceが¥10,000,000以上の場合' do
+        @item.price = 100000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+      it 'priceが¥300以下の場合' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+    end
+  end
 end
