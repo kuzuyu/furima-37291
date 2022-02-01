8c126298149da639cbf70bfd80f6098c4081d9f1
kuzuyu <95894334+kuzuyu@users.noreply.github.com>
2022-02-01 13:27:26 +0900

    コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :corrent_user, only: [:edit ]
-  before_action :set_items, only: [:edit, :show]
+  before_action :set_items, only: [:edit, :show, :update]
  
   def index
     @items = Item.all.order("created_at DESC")
@@ -27,7 +27,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
