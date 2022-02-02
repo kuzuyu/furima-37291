c20f64b3132b38a04cf9b253629bf642844e4674
kuzuyu <95894334+kuzuyu@users.noreply.github.com>
2022-02-02 13:59:42 +0900

    Update 20220202043536_create_orders.rb

--- db/migrate/20220202043536_create_orders.rb
+++ db/migrate/20220202043536_create_orders.rb
@@ -4,10 +4,10 @@ class CreateOrders < ActiveRecord::Migration[6.0]
 
       t.string     :post_number,          null: false
       t.integer    :area_id,              null: false
-      t.integer    :city,                 null: false
-      t.integer    :address,              null: false
-      t.integer    :building_name,        
-      t.integer    :phone_number,         null: false
+      t.string     :city,                 null: false
+      t.string     :address,              null: false
+      t.string     :building_name,        
+      t.string     :phone_number,         null: false
       t.references :record,               null: false, foreign_key: true
       t.timestamps
     end
