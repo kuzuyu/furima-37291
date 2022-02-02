b733b128ad947b1804d51e29dd21196cc7e444fc
kuzuyu <95894334+kuzuyu@users.noreply.github.com>
2022-02-02 13:38:16 +0900

    Formオブジェクトの作成

---
+++ app/models/order_records.rb
@@ -0,0 +1,9 @@
+class OrderRecords
+  include ActiveModel::Model
+  attr_accessor :post_number, :area_id, :city, :address, :building_name, :phone_number, :item, :user
+
+  def save
+    Order.create( post_number :post_number, area_id :area_id, address :address, building_name :building_name, phone_number :phone_number, record :record.id )
+    records = Records.create( user :user, item :item )
+  end
+end
