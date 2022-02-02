d1efa7517a62ced42746ab88753f4bd80ee695f0
kuzuyu <95894334+kuzuyu@users.noreply.github.com>
2022-02-02 14:19:31 +0900

    ordersテーブルの作り直し

--- db/migrate/20220202043536_create_orders.rb
+++ db/migrate/20220202043536_create_orders.rb
@@ -1,14 +1,12 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-
       t.string     :post_number,          null: false
       t.integer    :area_id,              null: false
       t.string     :city,                 null: false
       t.string     :address,              null: false
-      t.string     :building_name,        
+      t.string     :building_name
       t.string     :phone_number,         null: false
-      t.references :record,               null: false, foreign_key: true
       t.timestamps
     end
   end
