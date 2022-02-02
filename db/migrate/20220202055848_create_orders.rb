48c0b0c909bdda3ad2108aa995196fc736ba2ad8
kuzuyu <95894334+kuzuyu@users.noreply.github.com>
2022-02-02 15:00:05 +0900

    ordersテーブルの再作成

--- db/migrate/20220202055848_create_orders.rb
+++ db/migrate/20220202055848_create_orders.rb
@@ -7,6 +7,7 @@ class CreateOrders < ActiveRecord::Migration[6.0]
       t.string     :address,              null: false
       t.string     :building_name
       t.string     :phone_number,         null: false
+      t.references :record,               null: false, foreign_key: true
       t.timestamps
     end
   end
