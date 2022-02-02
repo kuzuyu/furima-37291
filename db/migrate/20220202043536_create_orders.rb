4b3ea42e6bd614543f1e7561bdb6cf5b5146c728
kuzuyu <95894334+kuzuyu@users.noreply.github.com>
2022-02-02 13:54:18 +0900

    orderテーブルのカラム作成

--- db/migrate/20220202043536_create_orders.rb
+++ db/migrate/20220202043536_create_orders.rb
@@ -2,6 +2,13 @@ class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
 
+      t.string     :post_number,          null: false
+      t.integer    :area_id,              null: false
+      t.integer    :city,                 null: false
+      t.integer    :address,              null: false
+      t.integer    :building_name,        
+      t.integer    :phone_number,         null: false
+      t.references :record,               null: false, foreign_key: true
       t.timestamps
     end
   end
