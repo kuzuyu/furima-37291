bc06154bcdf3146457d64ba5dc336dd5587328a1
kuzuyu <95894334+kuzuyu@users.noreply.github.com>
2022-02-03 03:46:23 +0900

    Update order_records.rb

--- app/models/order_records.rb
+++ app/models/order_records.rb
@@ -3,7 +3,7 @@ class OrderRecords
   attr_accessor :post_number, :area_id, :city, :address, :building_name, :phone_number, :item, :user
 
   def save
-    Order.create( post_number :post_number, area_id :area_id, address :address, building_name :building_name, phone_number :phone_number, record :record.id )
+    Order.create( post_number :post_number, area_id :area_id, address :address, building_name :building_name, phone_number :phone_number )
     records = Records.create( user :user, item :item )
   end
 end
