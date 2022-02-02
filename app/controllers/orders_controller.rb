bf3d36ebd8cd0e2713ddde5b9987f16d3a727ab8
kuzuyu <95894334+kuzuyu@users.noreply.github.com>
2022-02-02 15:28:22 +0900

    コントローラーの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,4 +3,13 @@ class OrdersController < ApplicationController
   def index
   end
 
+  def create
+    @order = OrderRecords.new(order_params)
+  end
+
+  private
+
+  def order_params
+    params.permit(:post_number, :area_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
