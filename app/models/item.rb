0be5b7b5e32183ef6979b549c45d68095fe1bdda
kuzuyu <95894334+kuzuyu@users.noreply.github.com>
2022-02-08 17:06:29 +0900

    コードの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
 
   validates :item_name, presence: true
   validates :item_information, presence: true 
-  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,  }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,  }
   validates :category_id, presence: true
   validates :item_condition_id, presence: true
   validates :postage_payer_id, presence: true
